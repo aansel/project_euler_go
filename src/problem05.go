d2c00bd6e82df526ec32cd489c66da13a90df224
aansel <antoine.ansel@gmail.com>
2014-12-26 12:36:09 +0100

    Problm 05

--- src/problem05.go
+++ src/problem05.go
@@ -6,19 +6,48 @@ import (
 
 
 func main() {
-	r := 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19
+	getSmallestMultiple(20)
+}
+
+
+func getSmallestMultiple(nb int) {
+	r, nonPrimes := getPrimesFactor(nb)
 	for i := r; ; i += r {
-		if isEvenlyDivisible(i, 20) {
+		ok := true
+		for _, nonPrime := range nonPrimes {
+			if (i % nonPrime != 0) {
+				ok = false
+				break
+			}
+		}
+		if ok {
 			fmt.Println(i)
 			break
 		}
 	}
 }
 
+/**
+ Le plus petit nombre divisible par tous les entiers de 1 à N a nécessaireement dans sa décomposition en facteurs premiers
+ au moins une fois tous les nombres premiers de 1 à N
+ */
+func getPrimesFactor(maxValue int) (int, []int) {
+	r := 1
+	var nonPrimes []int
+	for i := 2 ; i <= maxValue; i++ {
+		if isPrime(i) {
+			r *= i
+		} else {
+			nonPrimes = append(nonPrimes, i)
+		}
+	}
+	return r, nonPrimes
+}
 
-func isEvenlyDivisible(nb int, maxDivisor int) bool {
-	for i := maxDivisor ; i >= 2 ; i-- {
-		if nb % i != 0 {
+
+func isPrime(nb int) bool {
+	for i := 2 ; i < nb / 2 ; i++ {
+		if nb % i == 0 {
 			return false
 		}
 	}
